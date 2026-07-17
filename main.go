795b0cddec5cb193b72009f66bbd64932c6b5037
CamdenAJohnson <camden.johnson2021@gmail.com>
2026-07-17 13:09:20 -0700

    up

--- main.go
+++ main.go
@@ -1,8 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		fmt.Printf("Hits: %v\n", cfg.fileserverHits.Load())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	num := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %v", num)
+}
 
 func main() {
-	fmt.Printf("Hello World!")
+	logger := log.Default()
+
+	var apiCfg apiConfig
+
+	mux := http.NewServeMux()
+
+	fsHandler := http.FileServer(http.Dir("."))
+
+	metricsHandler := apiCfg.middlewareMetricsInc(fsHandler)
+
+	mux.Handle("/app/", http.StripPrefix("/app", metricsHandler))
+
+	mux.Handle("GET /metrics", &apiCfg)
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.Handle("POST /reset", apiCfg.middlewareMetricsReset(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})))
+
+	httpServer := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Println("Starting on port :8080")
+	if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Server error: %v\n", err)
+	}
 }
