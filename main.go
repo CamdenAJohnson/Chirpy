f5a050592e837a4708f8a7ca37daf1048e7a0831
CamdenAJohnson <camden.johnson2021@gmail.com>
2026-07-20 18:55:43 -0700

    Moved some more code around

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
-	"slices"
 	"time"
 
 	"github.com/CamdenAJohnson/Chirpy/internal/database"
@@ -24,40 +22,6 @@ type serverConfig struct {
 // Global Variables
 var ServerConfig serverConfig
 var APIConfig apiConfig
-
-// Helper functions
-
-// Middleware functon defintion
-type Middleware func(http.Handler) http.Handler
-
-// respondWithJson writes the given status code and json payload to http.ResponseWriter
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) error {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		return err
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Contorl-Allow-Origin", "*")
-	w.WriteHeader(code)
-	w.Write(response)
-	return nil
-}
-
-// respondWithError writes the status code and message into http.ResponseWriter
-func respondWithError(w http.ResponseWriter, code int, msg string) error {
-	return respondWithJson(w, code, map[string]string{"error": msg})
-}
-
-// CreateMiddlewareChain creates a chain of middleware functions
-func CreateMiddlewareChain(middlewares ...Middleware) Middleware {
-	return func(next http.Handler) http.Handler {
-		// Iterate backwards to preserve the natural execution order
-		for _, middleware := range slices.Backward(middlewares) {
-			next = middleware(next)
-		}
-		return next
-	}
-} 
 
 // main it's the main function
 func main() {
---
+++ util.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"slices"
+)
+
+// Middleware functon defintion
+type Middleware func(http.Handler) http.Handler
+
+// CreateMiddlewareChain creates a chain of middleware functions
+func CreateMiddlewareChain(middlewares ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		// Iterate backwards to preserve the natural execution order
+		for _, middleware := range slices.Backward(middlewares) {
+			next = middleware(next)
+		}
+		return next
+	}
+}
+
+// respondWithJson writes the given status code and json payload to http.ResponseWriter
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Contorl-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+// respondWithError writes the status code and message into http.ResponseWriter
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJson(w, code, map[string]string{"error": msg})
+}
